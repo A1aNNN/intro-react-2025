5c7f8a4530c498823a8712e38cc322c77638ac05
Alan Wang <alanwang123919@gmail.com>
2025-03-04 23:04:59 -0800

    updated Navbar file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,12 @@ import WebsiteLogo from '../images/website-logo.png';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar-brand">
-        <img 
-          src={WebsiteLogo}
-          alt="Nugget Logo"
-          className="navbar-logo-img"
-        />
-        <span className="navbar-logo-text">nugget.</span>
-      </div>
+      <img
+        src={WebsiteLogo}
+        alt="Nugget Logo"
+        className="navbar-logo-img"
+      />
+      <span className="navbar-logo-text">nugget.</span>
     </div>
   );
 };
